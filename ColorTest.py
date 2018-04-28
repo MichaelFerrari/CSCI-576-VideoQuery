5a9efd74f9652032eaf9bdd5ec0b7de579264e39
skywish <sky2wish@gmail.com>
2018-04-27 18:40:18 -0700

    Save data to npy, and compare

--- ColorTest.py
+++ ColorTest.py
@@ -1,6 +1,10 @@
 from PIL import Image
 import numpy as np
-from scipy.stats.stats import pearsonr
+import os
+import glob
+import time
+from scipy.stats import pearsonr
+from scipy.spatial.distance import cosine, euclidean, cityblock
 
 
 def read_img_color(file):
@@ -22,16 +26,123 @@ def read_img_color(file):
             # im2.putpixel((x, y), (r, g, b))
     # im2.save("1.png")
     # im2.show()
-    return comb
+    # print(comb.flatten())
+    return comb.flatten()
 
 
-def pearson_correlation_coefficients(comb1, comb2):
-    return np.corrcoef(comb1.flatten(), comb2.flatten())[0, 1]
+def read_file_img2(file_path, outfile_npy):
+    data_list = {}
+    for filename in glob.glob(os.path.join(file_path, '*.rgb')):
+        data_list[os.path.basename(filename)] = read_img_color(filename)
+    np.save(outfile_npy, data_list)
+
+
+def read_file_img(folder_path, folder_name, way):
+    data_list = []
+    # db 601, query 151
+    loop_time = 601 if way == 1 else 151
+    for _ in range(1, loop_time):
+        filename = folder_path + "/" + folder_name + "/" + folder_name + '{0:03}'.format(_) + ".rgb"
+        data_list.append(read_img_color(filename))
+    np.save(folder_name + ".npy", data_list)
+
+
+def read_query_file_and_compare(folder_path, folder_name, npy_file):
+    data_list = []
+    for _ in range(1, 151):
+        filename = folder_path + "/" + folder_name + "/" + folder_name + '{0:03}'.format(_) + ".rgb"
+        data_list.append(read_img_color(filename))
+    db_list = np.load(npy_file)
+    result = []
+    for i in range(0, 451):
+        temp = 0
+        for j in range(0, 15):
+            temp += calculate_similarity(2, data_list[10 * j], db_list[i + 10 * j])
+        result.append(temp)
+    print(result)
+
+
+def compare(query_npy, db_npy):
+    result = []
+    for i in range(0, 451):
+        temp = 0
+        for j in range(0, 150):
+            temp += calculate_similarity(1, query_npy[j], db_npy[i + j])
+        result.append(temp / 150)
+    print(result)
+
+
+def read_specific_npy_item(npy_path, file_basename):
+    n1 = np.load(npy_path)
+    return n1.item().get(file_basename)
+
+
+def calculate_similarity(way, comb1, comb2):
+    # https://docs.scipy.org/doc/scipy/reference/spatial.distance.html
+    # pearson_correlation_coefficients2
+    if way == 1:
+        return pearsonr(comb1, comb2)[0]
+        # return np.corrcoef(comb1, comb2)[0, 1]
+    # cosine_similarity
+    elif way == 2:
+        return 1 - cosine(comb1, comb2)
+    # Euclidean distance
+    elif way == 3:
+        return euclidean(comb1, comb2)
+    # City Block (Manhattan) distance.
+    elif way == 4:
+        return cityblock(comb1, comb2)
+
+
+def test_read_query_data(folder_path, folder_name):
+    query_data_list = []
+    for _ in range(1, 3):
+        filename = folder_path + "/" + folder_name + "/" + folder_name + '{0:03}'.format(_) + ".rgb"
+        query_data_list.append(read_img_color(filename))
+    print(query_data_list[0])
+
+
+def test_read_file_img():
+    start_time = time.time()
+
+    end_time = time.time()
+    print("it costs", end_time - start_time, "seconds")
+
+
+def test_read_specific_npy_item():
+    test_whole_path = "/Users/skywish/Downloads/Class/576/databse_videos/flowers/flowers001.rgb"
+    test_base_path = "flowers001.rgb"
+    print(read_specific_npy_item("flowers.npy", test_base_path))
+    print("========")
+    print(read_img_color(test_whole_path))
+
+
+def test_get_result(index):
+    n = np.load("traffic.npy")
+    print(n[index])
 
 
 if __name__ == "__main__":
-    file_path1 = "/Users/skywish/Downloads/Class/576/query/first/first001.rgb"
-    file_path2 = "/Users/skywish/Downloads/Class/576/query/first/first002.rgb"
-    result = pearson_correlation_coefficients(read_img_color(file_path1), read_img_color(file_path2))
-    print(result)
-    # read_img_color(file_path1)
+    test_db_file_path = "/Users/skywish/Downloads/Class/576/databse_videos"
+    test_db_file_name = "traffic"
+    test_query_file_path = "/Users/skywish/Downloads/Class/576/query"
+    test_query_file_name = "second"
+    npy_file_name = "traffic.npy"
+    traffic_npy = np.load("traffic.npy")
+    second_npy = np.load("second.npy")
+
+    t = time.time()
+    # print(pearsonr(second_npy[0], traffic_npy[0])[0])
+    compare(second_npy, traffic_npy)
+    print("It costs", time.time() - t, "seconds")
+
+    # t = time.time()
+    # print(re.findall('\d+', "first001.rgb")[0])
+    # test_get_result()
+    # test_read_specific_npy_item()
+    # test_read_file_img()
+    # f1 = "/Users/skywish/Downloads/Class/576/databse_videos/test"
+    # read_file_img(f1, "flowers.npy")
+    # result = pearson_correlation_coefficients(read_img_color(file_path1), read_img_color(file_path2))
+    # print(result)
+    # # read_img_color(file_path1)
--- GoogleTest.py
+++ GoogleTest.py
@@ -1,27 +1,43 @@
 import io
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/skywish/Downloads/CS576-Final-50c078a717bf.json"
-
-# Imports the Google Cloud client library
+import glob
+import csv
 from google.cloud import vision
 from google.cloud.vision import types
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/skywish/Downloads/CS576-Final-50c078a717bf.json"
 
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = "/Users/skywish/Projects/Python/CS576-Final/1.png"
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+def read_img_label(file_path, csv_path="/Users/skywish/Downloads/Class/576/img_labels.csv"):
+    for filename in glob.glob(os.path.join(file_path, '*.rgb')):
+        # Loads the image into memory
+        with io.open(filename, 'rb') as image_file:
+            content = image_file.read()
 
-image = types.Image(content=content)
+        image = types.Image(content=content)
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+        # Performs label detection on the image file
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+        with open(csv_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows("{0} ".format())
+        print(labels)
+        # print('Labels:')
+        # for label in labels:
+        #     print(label.description)
+
+
+if __name__ == "__main__":
+    # file_path1 = "/Users/skywish/Downloads/Class/576/databse_videos/flowers"
+    # read_img_label(file_path1)
+    with open("/Users/skywish/Downloads/Class/576/img_labels.csv", 'w', newline='') as f:
+        column1 = "File Names"
+        column2 = "Feature Values"
+        fieldnames = [column1, column2]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
